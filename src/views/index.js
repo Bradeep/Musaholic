28394cce9aff606f79613f96132f238b389902aa
Bradeep <bradeep001@gmail.com>
2023-08-05 01:20:57 +0530

    Integrate the spotify APIs to fetch suggestions

--- src/views/index.js
+++ src/views/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import NavBar from "../components/navBar";
 import "./index.scss";
@@ -6,8 +6,36 @@ import TextBar from "../components/Textbar";
 import Button from "../components/Button";
 
 import PlayIcon from "../assets/play.svg";
+import { debounce } from "../utils/utilFunctions";
+import { getRequest } from "../service/apiRequest";
+import { interceptor } from "../service/interceptor";
 
 const App = () => {
+  const [suggestions, setSuggestions] = useState([]);
+
+  useEffect(() => {
+    interceptor();
+  }, []);
+
+  const onSearch = debounce(async (value) => {
+    const requestUrl = `/v1/search?q=${value}&type=track,album&market=IN&include_external=audio&limit=8`;
+    try {
+      const res = await getRequest(requestUrl, null, {});
+      const data = res.data.tracks.items;
+      const resultArray = [];
+      Object.keys(data).forEach((each) => {
+        if (
+          ["single", "compilation", "album"].includes(
+            data[each].album.album_type
+          )
+        ) {
+          const name = data[each].name;
+          resultArray.push(name);
+        }
+      });
+      setSuggestions(resultArray);
+    } catch (error) {}
+  }, 500);
   return (
     <div className="wrapper">
       <NavBar title={"MUSAHOLIC"} />
@@ -28,7 +56,7 @@ const App = () => {
             className="content-icon--play"
             alt="play"
           />
-          <TextBar />
+          <TextBar onSearch={onSearch} suggestions={suggestions} />
           <div className="content-buttons">
             <Button buttonColor="rgb(30, 215, 96)">Submit</Button>
             <Button buttonColor="rgb(237, 95, 74)">{"Skip (+3s)"}</Button>
