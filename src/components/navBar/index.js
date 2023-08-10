c9eb7d64162a097b6d2da848a3da25e9cc0061cf
Bradeep <bradeep001@gmail.com>
2023-08-11 02:16:05 +0530

    Created Instruction set

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -1,6 +1,9 @@
 import React, { forwardRef, useState } from "react";
 import DatePicker from "react-datepicker";
 import Calendar from "../../assets/calendar.svg";
+import QuestionMark from "../../assets/question.svg";
+
+import Modal from "../Modal";
 
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -20,25 +23,57 @@ const NavBar = ({ title, onDateSelect }) => {
   ));
 
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [isInstructionOpen, setIsInstructionOpen] = useState(false);
 
-  const onChange = (date) => {
+  const onChangeDate = (date) => {
     setSelectedDate(date);
     onDateSelect(date);
+  };
+
+  const onModalClose = () => {
+    setIsInstructionOpen(false);
+  };
+
+  const onClickInfo = () => {
+    setIsInstructionOpen(true);
   };
   return (
     <div className="navWrapper">
       <div className="title-wrapper">
         <div>{title}</div>
       </div>
-      <div>
-        <DatePicker
-          selected={selectedDate}
-          dateFormat="dd/MM/yyyy"
-          filterDate={(e) => e < new Date() && e > new Date("2022-07-03")}
-          onChange={(date) => onChange(date)}
-          customInput={<CalendarInput />}
+      <div className="icons-wrapper">
+        <img
+          src={QuestionMark}
+          alt="question"
+          height={28}
+          width={28}
+          style={{ cursor: "pointer" }}
+          onClick={onClickInfo}
         />
+        <div>
+          <DatePicker
+            selected={selectedDate}
+            dateFormat="dd/MM/yyyy"
+            filterDate={(e) => e < new Date() && e > new Date("2022-07-03")}
+            onChange={(date) => onChangeDate(date)}
+            customInput={<CalendarInput />}
+          />
+        </div>
       </div>
+      <Modal
+        header={"Instructions"}
+        open={isInstructionOpen}
+        onClose={onModalClose}
+        modalCustomClass="nav-modal_class"
+        headerCustomId="nav-modal--title"
+      >
+        <div>
+          1. Play the audio for 3 seconds and guess the song from suggestion.
+        </div>
+        <div>2. If guessed wrongly , audio will be increased by 3 seconds.</div>
+        <div>3. Complete guessing within 4 choices and share your score.</div>
+      </Modal>
     </div>
   );
 };
