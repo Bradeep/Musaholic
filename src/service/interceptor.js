21cafbe28d6fb448b66e2a9e9ff155506ad83ebd
Bradeep <bradeep001@gmail.com>
2023-08-12 00:34:01 +0530

    Update token expiry logic

--- src/service/interceptor.js
+++ src/service/interceptor.js
@@ -1,5 +1,5 @@
 import api from "./api";
-import { postRequest } from "./apiRequest";
+import { postRequest, getRequest } from "./apiRequest";
 
 export function interceptor() {
   api.interceptors.request.use(
@@ -18,11 +18,14 @@ export function interceptor() {
       return response;
     },
     async function (error) {
+      let requestUrl = error.response.request.responseURL;
+
       if (error.response.status === 401) {
         const response = await postRequest("/api/token");
         if (response && response.status >= 200 && response.status <= 299) {
           const access_token = response?.data?.access_token;
           sessionStorage.setItem("access_token", access_token);
+          return await getRequest(requestUrl, null, {});
         }
       }
       return Promise.reject(error);
